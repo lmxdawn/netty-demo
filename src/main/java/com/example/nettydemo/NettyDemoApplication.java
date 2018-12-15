2af30c4043d40a2d6d04d4fce4cb2f5bebd2236d
lmxdawn@qq.com <lmxdawn@qq.com>
2018-12-15 09:28:27 +0800

    第一次提交

---
+++ src/main/java/com/example/nettydemo/NettyDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.nettydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyDemoApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/example/nettydemo/demo1/TextWebSocketFrameHandler.java
@@ -0,0 +1,42 @@
+package com.example.nettydemo.demo1;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelHandlerInvoker;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.time.LocalDateTime;
+
+//处理文本协议数据，处理TextWebSocketFrame类型的数据，websocket专门处理文本的frame就是TextWebSocketFrame
+public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    
+    //读到客户端的内容并且向客户端去写内容
+    @Override
+    protected void messageReceived(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        System.out.println("收到消息："+msg.text());
+        
+        /**
+         * writeAndFlush接收的参数类型是Object类型，但是一般我们都是要传入管道中传输数据的类型，比如我们当前的demo
+         * 传输的就是TextWebSocketFrame类型的数据
+         */
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("服务时间："+ LocalDateTime.now()));
+    }
+    
+    //每个channel都有一个唯一的id值
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        //打印出channel唯一值，asLongText方法是channel的id的全名
+        System.out.println("handlerAdded："+ctx.channel().id().asLongText());
+    }
+    
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerRemoved：" + ctx.channel().id().asLongText());
+    }
+    
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("异常发生");
+        ctx.close();
+    }
+}
